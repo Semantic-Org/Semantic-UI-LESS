35ae43acde6d65938317a7e3a3a577ac7ff0c206
Jack Lukic <jacklukic@gmail.com>
2017-07-11 08:59:03 -0700

    Updated distribution to version 2.2.11

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui',
   summary : 'Semantic UI - LESS Release of Semantic UI',
-  version : '2.2.10',
+  version : '2.2.11',
   git     : 'git://github.com/Semantic-Org/Semantic-UI-LESS.git',
 });
 
