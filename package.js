806bd67fcc445ce254a55a7dd153dae408f4485d
Pusher Robot <jack@semantic-ui.com>
2022-10-06 13:27:43 -0400

    Updated distribution to version 2.5.0

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui',
   summary : 'Semantic UI - LESS Release of Semantic UI',
-  version : '2.4.1',
+  version : '2.5.0',
   git     : 'git://github.com/Semantic-Org/Semantic-UI-LESS.git',
 });
 
